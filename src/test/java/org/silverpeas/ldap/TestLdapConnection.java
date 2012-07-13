1dc76784ef2491b87396b141face0470c50e05ad
Emmanuel Hugonnet <emmanuel.hugonnet@silverpeas.com>
2012-07-13 10:53:53 +0200

    Adding the license

--- src/test/java/org/silverpeas/ldap/TestLdapConnection.java
+++ src/test/java/org/silverpeas/ldap/TestLdapConnection.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (C) 2000 - 2012 Silverpeas
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.silverpeas.ldap;
 
 import java.util.Hashtable;
@@ -12,6 +26,7 @@ import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+
 /**
  * Unit test for simple App.
  */
@@ -24,7 +39,7 @@ public class TestLdapConnection {
 
   @Test
   public void testSimpleSearch() throws Exception {
-    Hashtable env = new Hashtable();
+    Hashtable<String, String> env = new Hashtable<String, String>(4);
     env.put(Context.INITIAL_CONTEXT_FACTORY,
         "com.sun.jndi.ldap.LdapCtxFactory");
     env.put(Context.PROVIDER_URL, "ldap://localhost:1389/dc=silverpeas,dc=org");
@@ -36,6 +51,6 @@ public class TestLdapConnection {
     assertThat(attrs.get("givenName").get().toString(), is("Nicolas"));
     assertThat(attrs.get("postalCode").get().toString(), is("50369"));
     assertThat(attrs.get("sn").get().toString(), is("EYSSERIC"));
-    
+
   }
 }
